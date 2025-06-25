7a759d5e9d550762a5cd49b3eec0b54e729a8225
richmondafari <afaririchmond1000@gmail.com>
2025-06-25 10:13:38 +0000

    chore: save in-progress work before branch switch

--- accounts/views.py
+++ accounts/views.py
@@ -6,25 +6,27 @@ from rest_framework import status
 
 class RegisterView(APIView):
     def post(self, request):
-        try:     #a try exception block to handle errors
-            data = request.data #get the data from the user
+        try:  # a try exception block to handle errors
+            data = request.data  # get the data from the user
 
-            serializer = RegisterSerializer(data=data) #create an instance of the register serializer class
+            # create an instance of the register serializer class
+            serializer = RegisterSerializer(data=data)
 
-            if not serializer.is_valid(): #validates if the data meets the requirements of the serializer class
+            if not serializer.is_valid():  # validates if the data meets the requirements of the serializer class
                 return Response({
                     'data': serializer.errors,
                     'message': 'Something Went Wrong'
-                }, status=status.HTTP_400_BAD_REQUEST) # if not return an error message 
+                }, status=status.HTTP_400_BAD_REQUEST)  # if not return an error message
 
-            serializer.save() #otherwise save the serializer created
+            serializer.save()  # otherwise save the serializer created
             return Response({
                 'data': {},
-                'message': 'your account is created' #and return a success message
+                'message': 'your account is created'  # and return a success message
 
             }, status=status.HTTP_201_CREATED)
 
-        except Exception as e: #catch unexpeted errors
+        except Exception as e:  # catch unexpeted errors
+            print(e)
             return Response({
                 'data': {},
                 'message': 'Something Went Wrong'
