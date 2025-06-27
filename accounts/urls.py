56acdfa0e9d6ca7d8993b14e3a6fe41d36fd7821
richmondafari <afaririchmond1000@gmail.com>
2025-06-27 12:09:17 +0000

    feat(auth): implement JWT authentication with registration and login

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,6 @@ from django.urls import path
 from accounts.views import RegisterView, LoginView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view()),
-    path('login/', LoginView.as_view()),
+    path('register/', RegisterView.as_view()), #route for registration
+    path('login/', LoginView.as_view()), #route for user to login
 ]
