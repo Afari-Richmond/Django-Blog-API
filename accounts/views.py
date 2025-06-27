de0e93bc1d04789ac84d84bd6f4bf7a644520096
richmondafari <afaririchmond1000@gmail.com>
2025-06-27 09:04:24 +0000

    comment(all): comment all the code

--- accounts/views.py
+++ accounts/views.py
@@ -33,22 +33,24 @@ class RegisterView(APIView):
             }, status=status.HTTP_400_BAD_REQUEST)
 
 
-class LoginView(APIView):
-    def post(self, request):
+class LoginView(APIView):    # Define The Login View Inside the API View Class
+    def post(self, request):  # Define a POST request because user will be entering data
 
         try:
-            data = request.data
+            data = request.data                  # store the data from the user in data
+            # pass the data into the login serializer and store it in the serializer variable
             serializer = LoginSerializer(data=data)
 
-            if not serializer.is_valid():
+            if not serializer.is_valid():  # validates if the data is correct or not using the serializer defined
                 return Response({
                     'data': serializer.errors,
                     'message': 'Something Went Wrong'
-                }, status=status.HTTP_400_BAD_REQUEST)
+                }, status=status.HTTP_400_BAD_REQUEST)  # if it's not, it returns an error message
 
-            response = serializer.get_jwt_token(serializer.data)
+            response = serializer.get_jwt_token(
+                serializer.data)  # if yes, it creates a JWT Token
+            # and returns a success message
             return Response(response, status=status.HTTP_200_OK)
-
 
         except Exception as e:  # catch unexpeted errors
             print(e)
