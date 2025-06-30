60c077c5918f5e43a0407d682fa9240b2b41b7f6
richmondafari <afaririchmond1000@gmail.com>
2025-06-30 13:26:13 +0000

    feat: Add a get functionality to get all blogs

--- home/views.py
+++ home/views.py
@@ -5,9 +5,44 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
-
+from django.core.paginator import Paginator
 from .models import Blog
 from django.db.models import Q
+
+
+class publicBlog(APIView):
+    def get(self, request):
+        try:
+            
+            blogs = Blog.objects.all().order_by('?')
+         
+
+            # checks if there's a query parameter named search  in the URL
+            if request.GET.get('search'):
+                # retrieves the actual value of search
+                search = request.GET.get("search")
+                # filters to check if the previously fetched
+                blogs = blogs.filter(
+                    Q(title_icontains=search) | Q(blogtext_icontains=search))
+                # data has the title or body as the value in the search field
+
+            page_number = request.GET.get('page', 1)
+            paginator = paginator(blogs, 5)
+            serializer = BlogSerializer(paginator.page(page_number), many= True)
+
+            return Response({
+                'data': serializer.data,
+                'message': 'blogs fetched successfully'  # and return a success message
+
+            }, status=status.HTTP_200_OK
+            )
+
+        except Exception as e:  # catch unexpeted errors
+            print(e)
+            return Response({
+                'data': {},
+                'message': 'Something Went Wrong'
+            }, status=status.HTTP_400_BAD_REQUEST)
 
 
 class BlogView(APIView):
@@ -110,5 +145,37 @@ class BlogView(APIView):
                 'data': {},
                 'message': 'Something Went Wrong'
             }, status=status.HTTP_400_BAD_REQUEST)
+        
+
+    def delete(self, request):
+        try:
+            data = request.data
+            blog = Blog.objects.filter(uid=data.get('uid'))
+
+            if not blog.exists():
+                return Response({
+                    'data': {},
+                    'message': 'invalid blog'
+                }, status=status.HTTP_400_BAD_REQUEST)
+
+            if request.user != blog[0].user:
+                return Response({
+                    'data': {},
+                    'message': 'you are not authorized to do this'
+                }, status=status.HTTP_400_BAD_REQUEST)
+            
+            blog[0].delete()
+            return Response({
+                'data': {},
+                'message': 'blog deleted successfully'  # and return a success message
+
+            }, status=status.HTTP_201_CREATED)
+        except Exception as e:  # catch unexpeted errors
+            print(e)
+            return Response({
+                'data': {},
+                'message': 'Something Went Wrong'
+            }, status=status.HTTP_400_BAD_REQUEST)
+
 
             
