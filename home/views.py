97cfb26105b350010bc54a505991ea2f557d17c0
richmondafari <afaririchmond1000@gmail.com>
2025-06-30 12:32:22 +0000

    feat(search): Add a search functionality to query specific blogs

--- home/views.py
+++ home/views.py
@@ -7,25 +7,36 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .models import Blog
+from django.db.models import Q
 
 
 class BlogView(APIView):
     permission_classes = [IsAuthenticated]
     authentication_classes = [JWTAuthentication]
 
-
-
     def get(self, request):
         try:
-            blogs = Blog.objects.filter(user = request.user)
+            # query the database to get all blog posts where the user
+            blogs = Blog.objects.filter(user=request.user)
+            # field matches the currently logged user
+
+            # checks if there's a query parameter named search  in the URL
+            if request.GET.get('search'):
+                # retrieves the actual value of search
+                search = request.GET.get("search")
+                # filters to check if the previously fetched
+                blogs = blogs.filter(Q(title_icontains=search) | Q(blogtext_icontains=search))
+                # data has the title or body as the value in the search field
+
             serializer = BlogSerializer(blogs, many=True)
 
             return Response({
                 'data': serializer.data,
                 'message': 'blogs fetched successfully'  # and return a success message
 
-            }, status=status.HTTP_201_CREATED)
-        
+            }, status=status.HTTP_200_OK
+            )
+
         except Exception as e:  # catch unexpeted errors
             print(e)
             return Response({
@@ -33,13 +44,10 @@ class BlogView(APIView):
                 'message': 'Something Went Wrong'
             }, status=status.HTTP_400_BAD_REQUEST)
 
-
-
     def post(self, request):
         try:
             data = request.data
-            data['user'] = request.user.id  
-
+            data['user'] = request.user.id
 
             serializer = BlogSerializer(data=data)
             if not serializer.is_valid():
